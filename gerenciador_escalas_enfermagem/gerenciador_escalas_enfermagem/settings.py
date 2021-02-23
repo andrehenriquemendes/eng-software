70dd8347def65b8fc7bf9f327b9dd698d097fa80
André Henrique Mendes <andre.mendes@outlook.com.br>
2021-02-23 20:47:44 -0300

    Edição settings.py

--- gerenciador_escalas_enfermagem/gerenciador_escalas_enfermagem/settings.py
+++ gerenciador_escalas_enfermagem/gerenciador_escalas_enfermagem/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == True
 
-ALLOWED_HOSTS = ['https://api-gerenciador-escalas.herokuapp.com/']
+ALLOWED_HOSTS = ['https://api-gerenciamento-escalas.herokuapp.com/']
 
 # Application definition
 
