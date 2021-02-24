f65531b6e4af53ffaa47ca10bd186cf1f35b7171
André Henrique Mendes <andre.mendes@outlook.com.br>
2021-02-23 23:27:23 -0300

    path

--- gerenciador_escalas_enfermagem/manage.py
+++ gerenciador_escalas_enfermagem/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciador_escalas_enfermagem.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciador_escalas_enfermagem.gerenciador_escalas_enfermagem.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
