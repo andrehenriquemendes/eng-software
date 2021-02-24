816d4240434349d1d7d38095ea388a55bc4ef2f6
André Henrique Mendes <andre.mendes@outlook.com.br>
2021-02-23 23:54:23 -0300

    update

--- gerenciador_escalas_enfermagem/gerenciador_escalas_enfermagem/settings.py
+++ gerenciador_escalas_enfermagem/gerenciador_escalas_enfermagem/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'd9eb5270f0a059305623b5ac47015d99b62f4ea6dbb83a05'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == True
